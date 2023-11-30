8c75fe581e1aedd901785b7be4da130b91a0551d
milkbreadzee <riaelisabeths@gmail.com>
2023-11-30 14:30:50 +0530

    taking user info

--- src/components/ClientReg/ClientReg.jsx
+++ src/components/ClientReg/ClientReg.jsx
@@ -20,44 +20,46 @@ import "./Creg.css"
 const ClientReg = () => {
     const [Cemail, setCemail] = useState(""); // State for email
     const [Cpassword, setCpassword] = useState("");
+    const [Cgender, setCgender] = useState("");
+    const [Cname, setCname] = useState("");
     const CconfirmPasswordRef = useRef(null);
     const [errorMsg, setErrorMsg] = useState("");
     const [msg, setMsg] = useState("");
     const [loading, setLoading] = useState(false);
 
-    const register = (email, password) =>
-    supabase.auth.signUp({ email, password });
+    const register = async (email, password, name, gender) => {
+  const { user, error } = await supabase.auth.signUp({ email, password });
+  if (error) {
+    throw error;
+  }
+  const { data, error: insertError } = await supabase
+    .from('users')
+    .insert([{ email, name, gender, id: user.id }]);
+  if (insertError) {
+    throw insertError;
+  }
+  return { user, data };
+};
 
-    const handleEmailChange = (e) => {
-        setCemail(e.target.value);
-      };
-    
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          setErrorMsg("");
-          setLoading(true);
-          //calling supabase register fucntion from above
-          const { data, error } = await register(
-            Cemail,
-            Cpassword
-          );
-          console.log(
-            Cemail,
-            Cpassword
-          )
-          if (!error && data) {
-            setMsg(
-              "Registration Successful. Check your email to confirm your account"
-            );
-          }
-        } catch (error) {
-          setErrorMsg("Error in Creating Account");
-          console.log(error)
-        }
-        setLoading(false);
-      };
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    setErrorMsg("");
+    setLoading(true);
+    const { user, data } = await register(
+      Cemail,
+      Cpassword,
+      Cname,
+      Cgender
+    );
+    console.log(user, data);
+    setMsg("Registration Successful. Check your email to confirm your account");
+  } catch (error) {
+    setErrorMsg("Error in Creating Account");
+    console.log(error);
+  }
+  setLoading(false);
+};
 
   return (
     <div className='CregForm'>
@@ -67,10 +69,10 @@ const ClientReg = () => {
             wrapperCol={{span : 14}}
             >
             <Form.Item name='fullName' label='Full Name' rules={[{required: true, message:'Please Enter Your Name'}]}>
-                <Input placeholder='Type your name'/>
+                <Input placeholder='Type your name' value={Cname} onChange={(e)=> setCname(e.target.value)}/>
             </Form.Item>
             <Form.Item name='email'  label='Email'>
-                <Input placeholder='Your email' value={Cemail} onChange={handleEmailChange}/>
+                <Input placeholder='Your email' value={Cemail} onChange={(e) => setCemail(e.target.value)}/>
             </Form.Item>
             <Form.Item name='password'  label='Password'>
                 <Input.Password value={Cpassword} onChange={(e) => setCpassword(e.target.value)} placeholder='password'/>
@@ -79,15 +81,15 @@ const ClientReg = () => {
                 <Input.Password placeholder='password'/>
             </Form.Item>
             <Form.Item name='gender' label='Gender'>
-                <Select placeholder='gender'>
+                <Select onChange={(value) => setCgender(value)} placeholder='gender'>
                     <Select.Option value='male'>Male</Select.Option>
                     <Select.Option value='female'>Female</Select.Option>
                     <Select.Option value='nospecify'>Choose not to specify</Select.Option>
                 </Select>
             </Form.Item>
-            <Form.Item name='dob' label='Date Of Birth'>
+            {/* <Form.Item name='dob' label='Date Of Birth'>
                 <DatePicker style={{width:'100%'}} picker='date' placeholder='Choose a date'/>
-            </Form.Item>
+            </Form.Item> */}
             <Form.Item name='tandc' wrapperCol={{span: 24}}>
                 <Checkbox> Agree to our <a href="#"> Terms and Conditons </a></Checkbox>
             </Form.Item>
